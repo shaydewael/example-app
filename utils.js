c033ebf30d8c5f4ef552cf5353a3189a66572d9f
Shay <shay.dewael@discordapp.com>
2024-03-06 17:23:25 -0500

    wtf shay

--- utils.js
+++ utils.js
@@ -47,7 +47,7 @@ export async function InstallGlobalCommands(appId, commands) {
 
   try {
     // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
-    await DiscordRequest(endpoint, { method: 'PUT' });
+    await DiscordRequest(endpoint, { method: 'PUT', body: commands });
   } catch (err) {
     console.error(err);
   }
